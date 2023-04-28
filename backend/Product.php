072e5c540014d711f716addfccb2ac6ed5bab62f
bolexs <boluwatufeadeojo@gmail.com>
2023-04-28 15:25:30 +0100

    refactoring

--- backend/Product.php
+++ backend/Product.php
@@ -40,18 +40,6 @@ abstract class Product
     }
     
     
-
-    // public  function saveProduct()
-    // {
-    //     $query = "INSERT INTO Products (sku, name, price, type) VALUES (:sku, :name, :price, :type)";
-    //     $this->db->query($query);
-    //     $this->db->bind(':sku', $this->getSku());
-    //     $this->db->bind(':name', $this->getName());
-    //     $this->db->bind(':price', $this->getPrice());
-    //     $this->db->bind(':type', $this->getType());
-    //     $this->db->execute();
-    // }
-
     public static function getAllProducts()
     {
         $db = new Database();
